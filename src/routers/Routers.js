6f2061e8c255735398ccfaeb1ec6209356e977a2
Enoch134 <men270992@gmail.com>
2023-04-24 18:25:15 +0000

    third commit

--- src/routers/Routers.js
+++ src/routers/Routers.js
@@ -8,9 +8,6 @@ import Blog from "../pages/Blog";
 import BlogDetails from "../pages/BlogDetails";
 import NotFound from "../pages/NotFound";
 import Contact from "../pages/Contact";
-import FreetownCars from "../components/UI/FreetownCars";
-import Province from "../components/UI/Province";
-import Freetown from "../components/UI/Freetown";
 import FreetownListing from "../pages/FreetownListing";
 import ProvinceListing from "../pages/ProvinceListing";
 import FreetownDetail from "../pages/FreetownDetail";
@@ -27,7 +24,6 @@ const Routers = () => {
       <Route path="/blogs" element={<Blog />} />
       <Route path="/blogs/:slug" element={<BlogDetails />} />
       <Route path="/contact" element={<Contact />} />
-      {/* <Route path="/freetownCars" element={<FreetownCars />} /> */}
       <Route path="/province" element={<ProvinceListing />} />
       <Route path="/province/:slug" element={<ProvinceDetail />} />
       <Route path="/freetown" element={<FreetownListing />} />
